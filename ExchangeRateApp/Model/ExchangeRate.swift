87909167455cd158c971cf9b87323745aba54ef6
JanisMuiznieks <43204658+JanisMuiznieks@users.noreply.github.com>
2021-05-04 15:06:02 +0300

    Update

--- ExchangeRateApp/Model/ExchangeRate.swift
+++ ExchangeRateApp/Model/ExchangeRate.swift
@@ -2,24 +2,17 @@
 //  ExchangeRate.swift
 //  ExchangeRateApp
 //
-<<<<<<< HEAD
 //  Created by janis.muiznieks on 04/05/2021.
-=======
+
 //  Created by e.vanags on 04/05/2021.
->>>>>>> 7dcbd6e31b5e4b9dcf36805229b7ed1765b9e874
-//
+
 
 import Foundation
 
 struct ExchangeRate {
-<<<<<<< HEAD
+
      let fromCurrency: String
      let toCurrency: String
      let rate: Double
  }
-=======
-    let fromCurrency: String
-    let toCurrency: String
-    let rate: Double
-}
->>>>>>> 7dcbd6e31b5e4b9dcf36805229b7ed1765b9e874
+
